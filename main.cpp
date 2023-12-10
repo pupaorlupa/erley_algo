fb35f11f61407437f7a1c0163e280a3d631065eb
dbelikov5 <dbelikov5@gmail.com>
2023-12-10 13:24:06 +0300

    final

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "LR1.h"
-#include "parser.h"
 #include <iostream>
 #include <memory>
 
+#include "LR1.h"
+#include "parser.h"
+
 int main() {
-  char name[20] = "../grammar.txt";
+  char name[1024] = GRAMMAR_PATH;
   std::shared_ptr<FileReader> reader = std::make_shared<FileReader>(name);
 
   Grammar gr;
@@ -19,3 +20,18 @@ int main() {
     std::cout << (checker.Predict(to_check) ? "YES" : "NO") << "\n";
   }
 }
+
+/* int main() { */
+/*   Grammar gr; */
+/*   std::shared_ptr<ConsoleReader> reader = std::make_shared<ConsoleReader>(); */
+/*   Parser parser; */
+/*   parser.Parse(reader, gr); */
+/*   LR1 checker(gr); */
+/*   int iterations; */
+/*   std::cin >> iterations; */
+/*   std::string to_check; */
+/*   for (int i = 0; i < iterations; ++i) { */
+/*     std::cin >> to_check(); */
+/*     std::cout << (checker.Predict(to_check) ? "YES" : "NO") << "\n"; */
+/*   } */
+/* } */
